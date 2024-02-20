e25b3ca97906af4a4f69a0a7c71ab761bdf8542b
utopica <h.elifokumus@gmail.com>
2024-02-20 23:08:45 +0300

    [Feat] Added session and customized Identity settings for password and user validation, utilizing the specified IdentityContext for entity framework stores.

--- Presentation/Flipard.MVC/Program.cs
+++ Presentation/Flipard.MVC/Program.cs
@@ -1,3 +1,4 @@
+using Flipard.Domain.Identity;
 using Flipard.Persistence.Contexts;
 using Flipard.Persistence.Contexts.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -19,7 +20,21 @@ builder.Services.AddDbContext<IdentityContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
-var app = builder.Build();
+builder.Services.AddSession();
+
+builder.Services.AddIdentity<User,Role>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*_-!%^&/=€$@";
+    options.User.RequireUniqueEmail = true;
+
+}).AddEntityFrameworkStores<IdentityContext>(); //U need to use my identitycontext
+
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -35,6 +50,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Session settings
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
