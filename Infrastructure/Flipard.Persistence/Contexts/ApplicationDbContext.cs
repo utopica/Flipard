9eb0393f900c21515320739063ee62450673fc98
utopica <h.elifokumus@gmail.com>
2024-02-20 21:42:16 +0300

    [Feat] Application DbContext is added to the project

---
+++ Infrastructure/Flipard.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Flipard.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipard.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Card>  Cards { get; set; }
+        public DbSet<Vocabulary> Vocabularies { get; set;}
+        public DbSet<Deck> Decks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+           
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
