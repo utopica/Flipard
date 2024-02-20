f796e94c51a05ea32c66dc7c100f10a683e12498
utopica <h.elifokumus@gmail.com>
2024-02-20 21:45:19 +0300

    [Feat] Identity structure is now forming, added Role, User, and IdentyContext

---
+++ Core/Flipard.Domain/Identity/User.cs
@@ -0,0 +1,23 @@
+﻿using Flipard.Domain.Common;
+using Flipard.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipard.Domain.Identity
+{
+    public class User :IdentityUser<Guid>, IEntityBase<Guid>, ICreatedByEntity, IModifiedByEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset? Birthdate { get; set; }
+        public Gender Gender { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string ModifiedByUserId { get; set; }
+    }
+}
---
+++ Infrastructure/Flipard.Persistence/Contexts/Identity/IdentityContext.cs
@@ -0,0 +1,27 @@
+﻿using Flipard.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipard.Persistence.Contexts.Identity
+{
+    public class IdentityContext : IdentityDbContext<User,Role,Guid>
+    {
+        //NO NEED TO CODE DBSETS 
+
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
