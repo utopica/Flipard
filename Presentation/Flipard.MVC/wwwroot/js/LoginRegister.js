acadd449ddeb4f4ba3b6bdc72ea52dcaff13ff97
V!ty <109697829+Vitalie03@users.noreply.github.com>
2024-03-22 18:36:17 +0000

    Login and Register Almost done

---
+++ Presentation/Flipard.MVC/wwwroot/js/LoginRegister.js
@@ -0,0 +1,8 @@
+
+document.getElementById("registerToLogin").addEventListener("click", function () {
+    window.location.href = "Login"; 
+});
+
+document.getElementById("loginToRegister").addEventListener("click", function () {
+    window.location.href = "Register"; 
+});
