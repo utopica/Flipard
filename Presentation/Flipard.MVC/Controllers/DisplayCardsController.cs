df8e40cbb583940dc34182dd19ba268624eb3446
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-05-22 13:04:21 +0300

    [feat] display page for flashcards has been created

---
+++ Presentation/Flipard.MVC/Controllers/DisplayCardsController.cs
@@ -0,0 +1,42 @@
+﻿using Flipard.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Flipard.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+public class FlashcardsController : Controller
+{
+    private readonly ApplicationDbContext _Appcontext;
+
+    public FlashcardsController(ApplicationDbContext appContext)
+    {
+        _Appcontext = appContext;
+    }
+
+    public IActionResult Index(Guid id)
+    {
+        var deck = _Appcontext.Decks
+            .Include(d => d.Cards)
+            .ThenInclude(c => c.Vocabulary)
+            .FirstOrDefault(d => d.Id == id);
+
+        if (deck == null)
+        {
+            return NotFound();
+        }
+
+        var model = new HomeCreateSetViewModel
+        {
+            Id = deck.Id,
+            Name = deck.Name,
+            Description = deck.Description,
+            TermMeanings = deck.Cards.Select(c => new TermMeaningViewModel
+            {
+                Id = c.Vocabulary.Id,
+                Term = c.Vocabulary.Term,
+                Meaning = c.Vocabulary.Meaning
+            }).ToList()
+        };
+
+        return View(model);
+    }
+}
