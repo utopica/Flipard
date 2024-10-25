f027b2633507ef8e44120d280f27aa88fdd1865d
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-10-25 22:15:45 +0300

    feat: added pie graph for quiz stats

---
+++ Infrastructure/Flipard.Persistence/Migrations/20241025161029_quiz.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flipard.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class quiz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuizAttempts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeckId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AttemptDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalQuestions = table.Column<int>(type: "integer", nullable: false),
+                    CorrectAnswers = table.Column<int>(type: "integer", nullable: false),
+                    Accuracy = table.Column<double>(type: "double precision", nullable: false),
+                    TimeTakenSeconds = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: true),
+                    DeletedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizAttempts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizAttempts_Decks_DeckId",
+                        column: x => x.DeckId,
+                        principalTable: "Decks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuizAttemptId = table.Column<Guid>(type: "uuid", nullable: false),
+                    VocabularyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserAnswer = table.Column<string>(type: "text", nullable: false),
+                    IsCorrect = table.Column<bool>(type: "boolean", nullable: false),
+                    QuizAttemptId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuizAttemptId2 = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: true),
+                    DeletedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedByUserId = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizAnswers_QuizAttempts_QuizAttemptId",
+                        column: x => x.QuizAttemptId,
+                        principalTable: "QuizAttempts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuizAnswers_QuizAttempts_QuizAttemptId1",
+                        column: x => x.QuizAttemptId1,
+                        principalTable: "QuizAttempts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuizAnswers_Vocabularies_VocabularyId",
+                        column: x => x.VocabularyId,
+                        principalTable: "Vocabularies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAnswers_QuizAttemptId",
+                table: "QuizAnswers",
+                column: "QuizAttemptId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAnswers_QuizAttemptId1",
+                table: "QuizAnswers",
+                column: "QuizAttemptId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAnswers_VocabularyId",
+                table: "QuizAnswers",
+                column: "VocabularyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAttempts_DeckId",
+                table: "QuizAttempts",
+                column: "DeckId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuizAnswers");
+
+            migrationBuilder.DropTable(
+                name: "QuizAttempts");
+        }
+    }
+}
